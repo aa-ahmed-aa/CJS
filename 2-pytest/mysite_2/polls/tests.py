fe0575de3a513273cc2679957ef7b183ae49fa84
root <root@localhost.localdomain>
2017-08-28 06:04:00 +0200

    Factory for groups and persons

--- 2-pytest/mysite_2/polls/tests.py
+++ 2-pytest/mysite_2/polls/tests.py
@@ -4,15 +4,10 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from datetime import datetime, time
 from django.http import HttpResponse
+from groupFactory import PersonFactory
 
-def test_greet(request):
-	now = datetime.now().time()
+def test_group_letter():
+	person = PersonFactory.build(
+		group__name='admins')
 
-	if time(5) < now < time(12):
-		greeting = 'morning'
-	elif time(18) < now < time(21):
-		greeting = 'evening'
-	else:
-		greeting = 'day'
-
-	return HttpResponse('Good %s!' % greeting)
+	assert person.group_letter() == 'A'
